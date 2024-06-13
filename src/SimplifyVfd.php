42e58f278d403229d18a221ec463cb6ed2535eb2
alphaolomi <alphaolomi@users.noreply.github.com>
2024-06-13 08:54:55 +0000

    Fix styling

--- src/SimplifyVfd.php
+++ src/SimplifyVfd.php
@@ -4,21 +4,19 @@ namespace Alphaolomi\SimplifyVfd;
 
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Psr7\Request;
-use GuzzleHttp\Exception\RequestException;
 
 class SimplifyVfd
 {
-
     private $client;
-    private $token = null;
 
+    private $token = null;
 
     public function __construct($config, $client = null)
     {
 
         $headers = [
             'Content-Type' => 'application/json',
-            'Accept' => 'application/json'
+            'Accept' => 'application/json',
         ];
 
         $this->client = $client ?? new GuzzleClient([
@@ -27,27 +25,24 @@ class SimplifyVfd
         ]);
     }
 
-
     /**
      * User Login
      *
-     * @param array $data{username: string, password: string}
-     *
+     * @param  array  $data{username:  string, password: string}
      * @return array{token: string, refresh_token: string}
      */
     public function userLogin($data)
     {
         $body = [
             'username' => $data['username'],
-            'password' => $data['password']
+            'password' => $data['password'],
         ];
 
-        $uri =  'https://stage.simplify.co.tz/partner/v1/auth/user/login';
+        $uri = 'https://stage.simplify.co.tz/partner/v1/auth/user/login';
 
         $request = new Request('POST', $uri, [], json_encode($body));
 
-        $resposne  = $this->client->sendAsync($request)->wait();
-
+        $resposne = $this->client->sendAsync($request)->wait();
 
         $responseBody = json_decode($resposne->getBody()->getContents(), true);
 
@@ -58,14 +53,10 @@ class SimplifyVfd
         return $responseBody;
     }
 
-
-
-
     /**
      * Create Issued Invoice
      *
-     * @param array $data
-     *
+     * @param  array  $data
      * @return array{success: int, invoiceId: string, verificationCode: string, verificationUrl: string, issuedAt: string}
      */
     public function createIssuedInvoice($data)
@@ -78,25 +69,24 @@ class SimplifyVfd
                 'vatRegistrationNumber' => $data['customer']['vatRegistrationNumber'],
                 'name' => $data['customer']['name'],
                 'mobileNumber' => $data['customer']['mobileNumber'],
-                'email' => $data['customer']['email']
+                'email' => $data['customer']['email'],
             ],
             'invoiceAmountType' => $data['invoiceAmountType'],
             'items' => $data['items'],
             'payments' => $data['payments'],
-            'partnerInvoiceId' => $this->generateGuid()
+            'partnerInvoiceId' => $this->generateGuid(),
         ];
 
-        $uri =  'https://stage.simplify.co.tz/partner/v1/invoice/createIssuedInvoice';
+        $uri = 'https://stage.simplify.co.tz/partner/v1/invoice/createIssuedInvoice';
 
         $request = new Request('POST', $uri, [
-            'Authorization' => 'Bearer ' . $this->token,
+            'Authorization' => 'Bearer '.$this->token,
         ], json_encode($body));
 
-        $resposne  = $this->client->sendAsync($request)->wait();
+        $resposne = $this->client->sendAsync($request)->wait();
 
         return json_decode($resposne->getBody()->getContents(), true);
     }
-
 
     /**
      * Generate GUID
@@ -109,33 +99,32 @@ class SimplifyVfd
     {
         return sprintf(
             '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0x0fff) | 0x4000,
-            mt_rand(0, 0x3fff) | 0x8000,
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
+            mt_rand(0, 0xFFFF),
+            mt_rand(0, 0xFFFF),
+            mt_rand(0, 0xFFFF),
+            mt_rand(0, 0x0FFF) | 0x4000,
+            mt_rand(0, 0x3FFF) | 0x8000,
+            mt_rand(0, 0xFFFF),
+            mt_rand(0, 0xFFFF),
+            mt_rand(0, 0xFFFF)
         );
     }
 
     /**
      * Get Invoice By Partner Invoice Id
      *
-     * @param string $partnerInvoiceId
-     *
+     * @param  string  $partnerInvoiceId
      * @return array
      */
     public function getInvoiceByPartnerInvoiceId($partnerInvoiceId)
     {
-        $uri =  'https://stage.simplify.co.tz/partner/v1/invoice/getInvoiceByPartnerInvoiceId/' . $partnerInvoiceId;
+        $uri = 'https://stage.simplify.co.tz/partner/v1/invoice/getInvoiceByPartnerInvoiceId/'.$partnerInvoiceId;
 
         $request = new Request('GET', $uri, [
-            'Authorization' => 'Bearer ' . $this->token,
+            'Authorization' => 'Bearer '.$this->token,
         ]);
 
-        $resposne  = $this->client->sendAsync($request)->wait();
+        $resposne = $this->client->sendAsync($request)->wait();
 
         return json_decode($resposne->getBody()->getContents(), true);
     }
